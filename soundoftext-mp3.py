3f9069fa3b78719a86454e7803728b94a887a4fc
Breno <breno@genova>
2018-10-22 15:07:41 -0300

    modify support japanese

--- soundoftext-mp3.py
+++ soundoftext-mp3.py
@@ -4,9 +4,16 @@ import sys
 
 API = "https://api.soundoftext.com/sounds"
 input_file = open(sys.argv[1],'r')
+language=sys.argv[2]
+if language == "en":
+    language="en-US"
+elif language == "jp":
+    language="ja-JP"
+else:
+    language="en-US"
 for word in input_file.readlines():
     filename='{0}.mp3'.format(word.rstrip())
-    data = {"engine":"Google","data":{"text":word,"voice":"en-US"}}
+    data = {"engine":"Google","data":{"text":word,"voice":language}}
     headers = {'Content-type': 'application/json', 'Accept': '*/*'}
     r = requests.post(API, data=json.dumps(data), headers=headers)
     id=json.loads(r.text)['id']
