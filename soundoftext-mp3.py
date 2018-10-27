0d40f5ff99fabbb44d3cf070046bd17e9930174f
ehauckdo <ehauckdo@uwaterloo.ca>
2018-10-27 01:25:58 -0400

    disable certificate verification

--- soundoftext-mp3.py
+++ soundoftext-mp3.py
@@ -15,10 +15,10 @@ for word in input_file.readlines():
     filename='{0}.mp3'.format(word.rstrip())
     data = {"engine":"Google","data":{"text":word,"voice":language}}
     headers = {'Content-type': 'application/json', 'Accept': '*/*'}
-    r = requests.post(API, data=json.dumps(data), headers=headers)
+    r = requests.post(API, data=json.dumps(data), headers=headers, verify=False)
     id=json.loads(r.text)['id']
     url = "{0}/{1}".format(API, id)
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=headers, verify=False)
     url_mp3=json.loads(r.text)['location']
     r=requests.get(url_mp3)
     open(filename, 'wb').write(r.content)
